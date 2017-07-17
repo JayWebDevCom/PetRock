ef5674f7dab02a405a34b49b12dc0b4ec7f4d6a5
JayWebDevCom <jaiye.s@gmail.com>
2017-07-17 17:50:00 +0100

    Tested

---
+++ tests/PetRockTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jaiye on 17/07/17.
+ */
+public class PetRockTest {
+
+    private PetRock rocky = new PetRock("Rocky");
+
+    @Test
+    public void getName() throws Exception {
+        assertEquals("Rocky", rocky.getName());
+    }
+
+    @Test
+    public void testHappyBeforePlay() throws Exception {
+        assertFalse(rocky.isHappy());
+    }
+
+    @Test
+    public void testHappyAfterPlay() throws Exception {
+        rocky.playWithToy();
+        assertTrue(rocky.isHappy());
+    }
+
+
+}
